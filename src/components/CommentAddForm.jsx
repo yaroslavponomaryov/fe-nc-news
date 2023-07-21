91d04c6730839de9d5b820007ed776ffe8d3eed6
Yaroslav Ponomaryov <yaroslavponomaryov@gmail.com>
2023-07-21 14:40:10 +0100

    comment adder: slow internet placeholder, alert if success

--- src/components/CommentAddForm.jsx
+++ src/components/CommentAddForm.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import { getCommentsForArticle, postComment } from "../api"
+import { useEffect, useState } from "react"
+import { postComment } from "../api"
 import validateComment from "../../utils/utils"
 const CommentAddForm = ({article_id, setArticleComments}) => {
 
     const [comment, setComment] = useState('')
+    const [isSending, setIsSending] = useState(false)
 
     const handleSubmit = (event) => {
         event.preventDefault()
@@ -18,25 +18,33 @@ const CommentAddForm = ({article_id, setArticleComments}) => {
                 created_at: new Date().toLocaleString()
             }]
         })
+        
         postComment(article_id, comment)
+            .then(()=>{
+                setIsSending(false)
+                alert('Thanks for commenting this article!')
+            })
             .catch((err) => {
-
+                setIsSending(false)
                 setArticleComments((currentComments)=> {
                     const removeLastComment = [...currentComments]
                     removeLastComment.pop()
-                    return removeComment})
+                    return removeLastComment
+                })
                 
                 alert(`We've got ${err.response.data.status} error: ${err.response.data.msg} :(`)
             })
 
     }
-    return (
+    
+    return isSending ? (<p>Your comment is on its way to be seen...</p>) : (
             <form className="form-floating mb-3 comment-box" onSubmit={(event)=>{
+                setIsSending(true)
                 handleSubmit(event);
                 document.getElementsByClassName('comment-sender-btn')[0].classList.add('disabled')
                 setComment('')
             }}>
-                <textarea className="form-control comment-text" id="floatingInput"  placeholder="This is a fab article! I like it very much!" value={comment} onChange={(event)=>{
+                <textarea className="form-control comment-text" id="floatingInput" placeHolder="This is a comment" value={comment} onChange={(event)=>{
                     setComment(event.target.value)
                     validateComment(comment)
                 }}/>
